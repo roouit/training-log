0f57dec06993eaf9ee4ceba8aa2fc95b10bd658b
Roope Uitto <roouit@gmail.com>
2021-12-15 18:57:40 +0200

    Promisify getting a connection from the mysql pool
    Get a connection, query database and release a connection without using
    callbacks. Will prevent callback hell when creating transactions.

--- src/api/v1/utils/db.js
+++ src/api/v1/utils/db.js
@@ -8,4 +8,34 @@ const config = {
   connectionLimit: 10
 }
 
-module.exports = mysql.createPool(config)
+const pool = mysql.createPool(config)
+
+/**
+ * A promisified function for getting a connection from the mysql connection pool.
+ * The returned connection also has promisified query and release functions.
+ * @returns {Promise} A promise that has a connection from mysql pool
+ */
+const connection = () => {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) reject(err)
+      const query = (sql, binding) => {
+        return new Promise((resolve, reject) => {
+          connection.query(sql, binding, (err, result) => {
+            if (err) reject(err)
+            resolve(result)
+          })
+        })
+      }
+      const release = () => {
+        return new Promise((resolve, reject) => {
+          if (err) reject(err)
+          resolve(connection.release())
+        })
+      }
+      resolve({ query, release })
+    })
+  })
+}
+
+module.exports = { pool, connection }

4bf793e02db4189295b4c05648b1f6df7ec06944
Roope Uitto <roouit@gmail.com>
2022-01-07 13:04:29 +0200

    Add page for unknown urls and redirect button to home page

--- src/frontend/src/core/router/router.component.js
+++ src/frontend/src/core/router/router.component.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import UserPage from '../../pages/user/'
 import SettingsPage from '../../pages/settings/'
 import AdminPage from '../../pages/admin/'
+import NotFoundPage from '../../pages/not-found/not-found.page'
 
 function Router () {
   return (
@@ -11,6 +12,7 @@ function Router () {
         <Route path='/' element={<UserPage />} />
         <Route path='/settings' element={<SettingsPage />} />
         <Route path='/admin' element={<AdminPage />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </>
   )
---
+++ src/frontend/src/pages/not-found/not-found.page.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import AdminNavigation from '../../core/app/navigation/admin'
+import { Link } from 'react-router-dom'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+
+function NotFoundPage() {
+  return (
+    <>
+      <AdminNavigation />
+      <Typography variant='h3' textAlign='center' gutterBottom={true}>
+        Page not found!
+      </Typography>
+      <Link to='/' style={{color: 'black', display: 'flex', justifyContent: 'center'}}>
+        <Button variant='outlined' color='inherit'>
+          Back to home page
+        </Button>
+      </Link>
+    </>
+  )
+}
+
+export default NotFoundPage

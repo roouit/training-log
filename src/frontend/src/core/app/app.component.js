b0e009aa4761f6f56c8472470fa52bd393ddea3a
Roope Uitto <roouit@gmail.com>
2022-01-08 10:37:39 +0200

    Install notification package and create reusable notification

--- src/frontend/src/core/app/app.component.js
+++ src/frontend/src/core/app/app.component.js
@@ -2,12 +2,14 @@ import React from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
 import Container from '@mui/material/Container'
 import Router from '../router/'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
     <>
       <CssBaseline />
       <Container maxWidth='md'>
+        <Toaster/>
         <Router />
       </Container>
     </>
---
+++ src/frontend/src/shared/components/toast/toast-notification.js
@@ -0,0 +1,33 @@
+import toast from 'react-hot-toast'
+
+function ToastNotification (success, message) {
+  return toast(
+    (t) => (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        <span>{message}</span>
+        <div 
+          onClick={() => toast.dismiss(t.id)} 
+          style={{
+            marginLeft: '10px',
+            cursor: 'pointer'
+          }}>
+            <b>X</b>
+        </div>
+      </div>
+    ),
+    {
+      duration: Infinity,
+      position: 'top-center',
+      style: {
+        border: `3px solid ${success ? 'green' : 'red'}`
+      }
+    }
+  )
+}
+
+export default ToastNotification
